b7b3509485ee79b3b47eca0151b3d5b3ed3e4a10
marvinam17 <121689133+marvinam17@users.noreply.github.com>
2024-02-03 18:57:30 +0100

    spark executes on worker now :)

--- start.bat
+++ start.bat
@@ -1,3 +1,3 @@
 docker-compose up -d --build
 
-docker-compose exec spark spark-submit --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.3.0,com.datastax.spark:spark-cassandra-connector_2.12:3.3.0 /src/main.py
+docker-compose exec spark sh -c "SPARK_MASTER_HOST=`hostname`; spark-submit --master spark://$SPARK_MASTER_HOST:7077 --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.3.0,com.datastax.spark:spark-cassandra-connector_2.12:3.3.0 /src/main.py"
